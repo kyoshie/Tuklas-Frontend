d46a179cb4adaf44f9d3bdd166faa160bbb7f14d
Kyoshie <157869727+kyoshie@users.noreply.github.com>
2024-06-03 12:21:58 +0000

    updated home page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,18 +4,16 @@ import './Intro.css'
 const Intro = () => {
     return (
       <div className='welcome'>
-      <div className='inner'>
-          <h3> Welcome To Tuklas Art Gallery</h3>
+        <div className='inner'>
+          <h3>Welcome To Tuklas Art Gallery</h3>
           <div className='text'>
-              <p>Welcome to Tuklas Art Gallery, a digital platform dedicated to showcasing the artistic talent of creators from Batangas province.</p>
-              <p>Our gallery provides a space for local artists to exhibit their work, connect with art enthusiasts, and explore the exciting world of NFTs (Non-Fungible Tokens).</p>
-              <p> Artists can upload their creations, whether it be paintings, sculptures, or digital art, and transform them into NFTs, allowing for unique ownership and authenticity in the digital realm.</p>    
-              <p>Join us in celebrating the vibrant art scene of Batangas and discover the endless possibilities of merging traditional art with cutting-edge technology at Tuklas Art Gallery. </p>
+              <p>Discover the artistic talent of Batangas province.</p>
+              <p>Explore, connect, and engage with local art and NFTs.</p>
+              <p>Celebrate the vibrant art scene with us at Tuklas Art Gallery.</p>
           </div>
+        </div>
       </div>
-  </div>
     )
-
 }
 
 export default Intro
