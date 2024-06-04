df15aeb5644f7b10a2052d07ef2a85382704450a
Kyoshie <157869727+kyoshie@users.noreply.github.com>
2024-06-04 03:19:35 +0000

    admin profile updated, still lacking

--- src/pages/AdminProfile/AdminProfile.jsx
+++ src/pages/AdminProfile/AdminProfile.jsx
@@ -1,29 +1,23 @@
 import './AdminProfile.css';
+import { Link } from 'react-router-dom';
 
 const AdminProfile = () => {
     return (
         <div className="admin-profile"> 
             <div className='admin-info'>
+                <img className='image-profile' src="gojo.jpg" alt="Profile Image" />
+                <h1 className='admin-name'>Team Uruk</h1>
+                <h2 className='role'>Admin</h2>
+                <p className='information'> This is Team Uruk admin profile.</p>
+                <br></br>
                 
 
-
-
-
+                <Link to="/arts" className='see-arts'>
+                    View Arts
+                </Link>
             </div>
         </div>
-
-
-
-
-
-
-    )
-
-
-
-
-
+    );
 }
 
-
-export default AdminProfile
+export default AdminProfile;
